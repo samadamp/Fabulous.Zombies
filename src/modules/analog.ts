663a722029999ddd494878104a12c7037f9d287a
samadamp <samadampankhurst@gmail.comgit config --global user.name samadampgit statusgit config --global user.email samadampankhurst@gmail.com>
2024-01-19 13:43:24 +0100

    heej

--- src/modules/analog.ts
+++ src/modules/analog.ts
@@ -1,4 +1,12 @@
 //funktionerna för analoga timer
+// först och främst få ut i konsolen vad tiden är nu.
+// Funktion för att bestäma tid
+// Knapp för att starta tid
+
+// Sen vad är tiden om 10 minuter 
+// När time is up så den vet att den sluta räkna
+// När det är gjort så paus
+// Abort
 function myfunction(){
     console.log("hello");
     anotherFunction();
