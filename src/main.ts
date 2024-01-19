d0c971d19482855786a897a7dddd83d41b476344
samadamp <samadampankhurst@gmail.comgit config --global user.name samadampgit statusgit config --global user.email samadampankhurst@gmail.com>
2024-01-19 09:01:22 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,9 @@ todos.forEach((todo) => {
   div.innerHTML = `label: ${todo.label} - done: ${todo.done}`;
   app.append(div);
 });
+
+
+import moment from 'moment';
+
+const now = moment();
+console.log(now.format('YYYY-MM-DD HH:mm:ss'));
