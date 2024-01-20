52b44251965d896060d32c850a82e795d1d96847
Robin <robin_nygren@outlook.com>
2024-01-20 13:37:41 +0100

    analog timer not quite working yet

--- src/main.ts
+++ src/main.ts
@@ -4,55 +4,45 @@ import analogfunctions from "./modules/analog";
 
 analogfunctions.myfunction();
 analogfunctions.newFunction();
+analogfunctions.setTimer();
+
+const startTimerBtn: HTMLButtonElement | null = document.getElementById('startTimerBtn') as HTMLButtonElement;
+
+if (startTimerBtn) {
+    startTimerBtn.addEventListener('click', () => {
+      analogfunctions.setTimer();
+    });
+}
 
 
-const now = moment();
-console.log(now.format('YYYY-MM-DD HH:mm:ss'));
+/* const now = moment();
+console.log(now.format('YYYY-MM-DD HH:mm:ss')); */
 
 // Funktion för att starta en timer
-function startTimer(durationInMinutes: number) {
+/* function startTimer(durationInMinutes: number) {
     const startTime = moment();
-    const endTime = moment().add(durationInMinutes, 'minutes');
+    const endTime = moment().add(durationInMinutes, 'minutes'); */
 
     // Uppdatera varje sekund
-    const timerInterval = setInterval(() => {
+    /* const timerInterval = setInterval(() => {
         const currentTime = moment();
-        const remainingTime = moment.duration(endTime.diff(currentTime));
+        const remainingTime = moment.duration(endTime.diff(currentTime)); */
 
         // Visa återstående tid
-        console.log(`Återstående tid: ${remainingTime.minutes()} minuter och ${remainingTime.seconds()} sekunder`);
-
+/*         console.log(`Återstående tid: ${remainingTime.minutes()} minuter och ${remainingTime.seconds()} sekunder`);
+ */
         // Om tiden har gått ut, avsluta timern
-        if (currentTime.isAfter(endTime)) {
+       /*  if (currentTime.isAfter(endTime)) {
             clearInterval(timerInterval);
             console.log('Timer avslutad!');
         }
     }, 1000); // Uppdatera varje sekund
 }
-
+ */
 // Exempel: Starta en timer på 5 minuter
-startTimer(5);
+/* startTimer(5); */
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
 
-interface Todo {
-  id: number;
-  label: string;
-  description?: string;
-  done: boolean;
-}
-
-const todos: Todo[] = [
-  { id: 0, label: "buy groceries", done: true },
-  { id: 1, label: "buy eggs", done: false },
-  { id: 2, label: "sleep", done: true },
-];
-
-todos.forEach((todo) => {
-  const div = document.createElement("div");
-  div.innerHTML = `label: ${todo.label} - done: ${todo.done}`;
-  app.append(div);
-});
 
 
 
--- src/modules/analog.ts
+++ src/modules/analog.ts
@@ -1,4 +1,24 @@
 //funktionerna för analoga timer
+import moment from 'moment';
+
+function setTimer(){
+    const deg: number = 6;
+    const min: HTMLElement | null = document.querySelector('.min');
+    const sec: HTMLElement | null = document.querySelector('.sec');
+    const timerInput: string = (<HTMLInputElement>document.getElementById('timerInput')).value;
+    const timerMoment: moment.Moment = moment(timerInput, 'mm:ss');
+    const mm: number = timerMoment.minutes() * deg;
+    const ss: number = timerMoment.seconds() * deg;
+
+
+    if (min && sec) {
+        min.style.transform = `rotateZ(${mm}deg)`;
+        sec.style.transform = `rotateZ(${ss}deg)`;
+        console.log(`Transform updated - Minutes: ${mm}, Seconds: ${ss}`);
+    
+    }
+}
+
 function myfunction(){
     console.log("hello");
     anotherFunction();
@@ -16,5 +36,5 @@ function anotherFunction (){
 }
 
 
-export default {myfunction, newFunction}
+export default {myfunction, newFunction, setTimer}
 
