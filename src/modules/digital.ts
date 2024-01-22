ad2c5a4d86c33b578fcce4336cbb34468674f6c7
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-01-22 10:50:59 +0100

    First

--- src/modules/digital.ts
+++ src/modules/digital.ts
@@ -1,12 +1,14 @@
 import moment from 'moment';
 
 let startTime: moment.Moment | null = null;
+// event listener drop down button click overlay
+//event listener abort button 'click'
 
 function updateTimer() {
   if (startTime) {
     const currentTime = moment();
     const duration = moment.duration(currentTime.diff(startTime));
-    const formattedTime = moment.utc(duration.asMilliseconds()).format('HH:mm:ss');
+    const formattedTime = moment.utc(duration.asMilliseconds()).format('mm:ss');
     document.getElementById('timer')!.innerText = formattedTime;
   }
 }
