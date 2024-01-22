132f56651d873830ffa3850541f1b15eb8ae6ec5
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-01-22 21:11:51 +0100

    setTimePscode

--- src/modules/settimer.ts
+++ src/modules/settimer.ts
@@ -1,11 +1,21 @@
 // function setTimer
 
-var unchangedTime = 10;
-var choosenTime;
+let unchangedTime = 10;
+let choosenTime ;
+//event listener onclick leftbutton
+//event listener onclick rightbutton
+// When click on left choosenTime = unchangedTime - 1
+// When click on right choosenTime = unchangedTime + 1
+// while choosenTime 0< || > 60, keep adding/reducing min 
     
 
-function setTimer(){
+function setTimer(choosenTime: number){
 
+//if left button click
 
+// if right button click
+
+//if else 
+choosenTime = unchangedTime;
 }
 // 
