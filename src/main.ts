14f109c38f2a308a2cdd35961ad4a8f6a86f109e
samadamp <samadampankhurst@gmail.comgit config --global user.name samadampgit statusgit config --global user.email samadampankhurst@gmail.com>
2024-01-23 13:38:33 +0100

    removed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import "./scss/styles.scss";
+import moment from 'moment';
 
-import timerFunctions from "./modules/settimer";
+import { initializeTimer } from "./modules/settimer";
+
+
+initializeTimer({ containerId: 'timers-container', startButtonId: 'startButtonId', stopButtonId: 'stopButtonId' });
+
 
 
 
@@ -31,26 +36,6 @@ function startTimer(durationInMinutes: number) {
 // Exempel: Starta en timer på 5 minuter
 startTimer(5);
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
-
-interface Todo {
-  id: number;
-  label: string;
-  description?: string;
-  done: boolean;
-}
-
-const todos: Todo[] = [
-  { id: 0, label: "buy groceries", done: true },
-  { id: 1, label: "buy eggs", done: false },
-  { id: 2, label: "sleep", done: true },
-];
-
-todos.forEach((todo) => {
-  const div = document.createElement("div");
-  div.innerHTML = `label: ${todo.label} - done: ${todo.done}`;
-  app.append(div);
-});
 
 
 
