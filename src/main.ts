a589fcc92b3baff920917bc7a105f2812b161a16
samadamp <samadampankhurst@gmail.comgit config --global user.name samadampgit statusgit config --global user.email samadampankhurst@gmail.com>
2024-01-24 12:40:49 +0100

    updated submain

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./scss/styles.scss";
 import moment from 'moment';
+import analogfunctions from "./modules/analog";
+analogfunctions.startAnalogTimer();
 
 const handleTimerButtonClick = (timerStyle: string): void => {
   // Dölj alla timers
@@ -55,11 +57,9 @@ timerBtns.forEach((btn) => {
   });
 });
 
-import "./scss/styles.scss";
-import moment from 'moment';
-import analogfunctions from "./modules/analog";
 
-analogfunctions.startAnalogTimer();
+
+
 
 const startTimerBtn: HTMLButtonElement | null = document.getElementById('startTimerBtn') as HTMLButtonElement;
 
@@ -67,34 +67,6 @@ if (startTimerBtn) {
     startTimerBtn.addEventListener('click', () => {
     });
 }
-
-
-/* const now = moment();
-console.log(now.format('YYYY-MM-DD HH:mm:ss')); */
-
-// Funktion för att starta en timer
-/* function startTimer(durationInMinutes: number) {
-    const startTime = moment();
-    const endTime = moment().add(durationInMinutes, 'minutes'); */
-
-    // Uppdatera varje sekund
-    /* const timerInterval = setInterval(() => {
-        const currentTime = moment();
-        const remainingTime = moment.duration(endTime.diff(currentTime)); */
-
-        // Visa återstående tid
-/*         console.log(`Återstående tid: ${remainingTime.minutes()} minuter och ${remainingTime.seconds()} sekunder`);
- */
-        // Om tiden har gått ut, avsluta timern
-       /*  if (currentTime.isAfter(endTime)) {
-            clearInterval(timerInterval);
-            console.log('Timer avslutad!');
-        }
-    }, 1000); // Uppdatera varje sekund
-}
- */
-// Exempel: Starta en timer på 5 minuter
-/* startTimer(5); */
 
 
 
