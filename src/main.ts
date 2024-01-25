673d33fd270be21ba6128a041b1f7905b2bfe03a
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-01-25 14:06:22 +0100

    removed

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,8 @@ import digitalfunctions from "./modules/digital";
 digitalfunctions.updateTimer();
 digitalfunctions.startTimer();
 digitalfunctions.stopTimer();
-import timerModule from './modules/settimer';
 
-timerModule.createTimerElements();
+
 
 
 
